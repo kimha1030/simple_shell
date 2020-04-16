67b3b06f8bd2cc4d8c9da660cf48e27ffe8ef739
peluza <edisonisaza@gmail.com>
2020-04-16 12:11:35 -0500

    get_arr

---
+++ getarr.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * **getarr - Function that allows to get the command and arguments 
+ * @buffer: pointer to string
+ * @len: lenght of input
+ **/
+
+char **getarr(char *buffer, int len)
+{
+	char **arr, *token, *value;
+	int x;
+
+	arr = malloc(len * sizeof(char *));
+	if (arr == NULL)
+		perror("error: ");
+	token = _strdup(buffer);
+	value = _strtok(token, " \n\t");
+	x = 0;
+	while (value != NULL)
+	{
+		arr[x] = _strdup(value);
+		value = _strtok(NULL, " \n\t");
+		x++;
+	}
+	return (arr);
+	free(arr);
+	free(token);
+	free(value);
+}
