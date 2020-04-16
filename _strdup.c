06915a4e54aac1f67c08b3eeedba77d411328dd8
kimha1030 <1478@holbertonschool.com>
2020-04-16 15:31:17 -0500

    Function _strdup

---
+++ _strdup.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ * *_strdup - returns a pointer to a newly space in memory.
+ * @s: pointer to string
+ * Return: pointer to string n
+ **/
+
+char *_strdup(const char *s)
+{
+	int x = 0, y = 0;
+	char *n;
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		while (s[x] != '\0')
+		{
+			x++;
+		}
+		n = malloc(sizeof(char) * (x + 1));
+		if (n == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			while (s[y] != '\0')
+			{
+				n[y] = s[y];
+				y++;
+			}
+			return (n);
+			free(n);
+		}
+	}
+}
