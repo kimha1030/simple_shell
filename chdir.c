b976bacbf6ece1f805da3433f91060a62cf4fb38
kimha1030 <1478@holbertonschool.com>
2020-04-16 15:50:31 -0500

    Function chdir

---
+++ chdir.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * chdir - Function that move to current directory
+ * @args: list of variable arguments
+ * Return: Always 0.
+ **/
+
+int _chdir(va_list args)
+{
+	printf("path\n");
+	return (0);
+	free(args);
+}
