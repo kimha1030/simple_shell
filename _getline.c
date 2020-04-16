8a0c06b6f23a7a3717d6c757c9ac4b3a5da81e55
kimha1030 <1478@holbertonschool.com>
2020-04-16 12:06:46 -0500

    Function _getline

---
+++ _getline.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * _getline - Function that allows to get the input
+ * 
+ * Return: buffer.
+ **/
+
+char *_getline(void)
+{
+	ssize_t tmp;
+	char *buffer = NULL;
+	size_t i  = 0;
+
+	tmp = getline(&buffer, &i, stdin);
+	fflush(stdin);
+	if (tmp == EOF)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			write(1, "\n", 1);
+		}
+		free(buffer);
+		exit(0);
+	}
+	return (buffer);
+}
+
