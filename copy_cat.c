1b03cdda203a8b5f3d79325118a1cb5221e5e76b
peluza <edisonisaza@gmail.com>
2020-04-16 15:44:24 -0500

    copy_cat

---
+++ copy_cat.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+
+/**
+ * copy_cat - copy and cat
+ * @len: is long of the characters
+ * @arr: the charaters is the definitive of the call a the funtion
+ * Return: dest
+ */
+
+void copy_cat(int len, char **arr)
+{
+	char *str, *strc, argv[] = {"/bin/"};
+	int resul;
+
+	str = malloc(len * sizeof(char));
+	if (str == NULL)
+		perror("error: ");
+	_strcpy(str, argv);
+	resul = find_char(arr[0], '/');
+	if (resul == 0)
+	{
+		strc = malloc(len * sizeof(char));
+		if (strc == NULL)
+			perror("error: ");
+		strc = _strrchr(arr[0], '/');
+		_strcat(str, strc);
+	}
+	else
+	{
+		_strcat(str, arr[0]);
+	}
+	if (stat(str, &st) == 0)
+	{
+		if (execve(str, arr, NULL) == -1)
+		{
+			perror("error. ");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		fprintf(stderr, "%s: No such file or directory\n", arr[0]);
+		exit(EXIT_FAILURE);
+	}
+	free(str);
+	free(strc);
+}
