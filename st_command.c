d891fea0866f666d6fc7aae12b43c10fd7bf7bc3
kimha1030 <1478@holbertonschool.com>
2020-04-16 21:50:43 -0500

    Update function st_command

--- st_command.c
+++ st_command.c
@@ -1,24 +1,22 @@
 #include "holberton.h"
 
 /**
- * st_command - Function that allows us to choose the cd command or exit
- * @buffer1: pointer to string
+ * st_command - Compare the buffer with exit or pass to another process
+ * @buffer: pointer to string
  * Return: 0
  **/
-#include "holberton.h"
 
-int st_command(char *buffer1)
+int st_command(char *buffer)
 {
-	if (strcmp(buffer1, "exit\n") == 0)
+	if (strcmp(buffer, "exit\n") == 0)
 	{
-
-		free(buffer1);
-		buffer1 = NULL;
+		free(buffer);
+		buffer = NULL;
 		exit(0);
 	}
-	else if (strcmp(buffer1, "exit\n") != 0)
+	else if (strcmp(buffer, "exit\n") != 0)
 	{
-		process(buffer1);
+		process(buffer);
 	}
 	return (0);
 }
